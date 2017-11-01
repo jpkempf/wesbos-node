2c4b3a95f9a24b0f373402bece84f88c2d1fa48a
Kempf, Jan-Philipp <J.Kempf@douglas.de>
2017-11-01 19:46:01 +0100

    lesson 11

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,24 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+/**
+ * HOME PAGE
+ */
+
 exports.homePage = (req, res) => {
   res.render('index');
+};
+
+/**
+ * ADD & EDIT STORES
+ */
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add store' });
+};
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
+/**
+ * HOME PAGE
+ * PATH: /
+ */
 
 router.get('/', storeController.homePage);
+
+/**
+ * ADD & EDIT STORES
+ * PATH: /add
+ */
+
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
